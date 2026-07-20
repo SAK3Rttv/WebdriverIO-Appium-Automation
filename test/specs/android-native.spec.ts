c915ad6d00b9a1f116182210e806ee18b46da90b
Abdullah Muhaisen <engabdullahsak@gmail.com>
2026-07-20 22:52:19 +0300

    Android Native Feature - 02 (Dialog Alert)

--- test/specs/android-native.spec.ts
+++ test/specs/android-native.spec.ts
@@ -4,13 +4,29 @@ describe('Android Native Feature Tests', () => {
             "io.appium.android.apis",
             "io.appium.android.apis.app.AlertDialogSamples");
 
-
-        // Debug pause screen 3s
-        await driver.pause(3000);
-
-
         // Assertion
         await expect($('//*[@text="App/Alert Dialogs"]')).toBeExisting ();
 
+    })
+
+    it('Working with Dialog Boxes', async () => {
+
+        // 1) *** Click on the first dialog by id= ***
+        // await $('id=io.appium.android.apis:id/two_buttons').click();
+    //-------------------------------------------------------------------------------------------------
+        // 2) *** Click on first dialog by xpath ***
+        // await $('//android.widget.Button[@resource-id="io.appium.android.apis:id/two_buttons"]').click();
+    //-------------------------------------------------------------------------------------------------
+        // 3) *** Click on first dialog by UiSelector ***
+        // await $('android=new UiSelector().resourceId("io.appium.android.apis:id/two_buttons")').click();
+
+        // Click on the first dialog by id=
+        await $('id=two_buttons').click();
+
+        // accept Alert
+        await driver.acceptAlert();
+
+        // assertion - alert is no longer present
+        await expect($('id=alertTitle')).not.toBeExisting();
     })
 })
