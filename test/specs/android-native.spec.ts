eb3035aa69872fc90c07aa47aaaec8c8a07ed39a
Abdullah Muhaisen <engabdullahsak@gmail.com>
2026-07-21 11:22:36 +0300

    Android Native Feature - Scrolling

--- test/specs/android-native.spec.ts
+++ test/specs/android-native.spec.ts
@@ -29,4 +29,56 @@ describe('Android Native Feature Tests', () => {
         // assertion - alert is no longer present
         await expect($('id=alertTitle')).not.toBeExisting();
     })
+
+    it('Vertical scrolling', async () => {
+        await $('~App').click();
+        await $('~Activity').click();
+
+
+        // scroll to the end (not stable if elements gets moved)
+        // await $('android=new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(1,5)');
+
+        // scroll to an element with text "Secure Surfaces"
+        await $('android=new UiScrollable(new UiSelector().scrollable(true)).scrollTextIntoView("Secure Surfaces")').click();
+
+        // assertion
+        await expect($('~Secure Dialog')).toBeExisting();
+    })
+
+    it('Horizontal scrolling', async () => {
+        await driver.startActivity(
+            "io.appium.android.apis",
+            "io.appium.android.apis.view.Gallery1");
+
+        // Horizontal scrolling
+        await $('android=new UiScrollable(new UiSelector().scrollable(true)).setAsHorizontalList().scrollForward()');
+        await $('android=new UiScrollable(new UiSelector().scrollable(true)).setAsHorizontalList().scrollBackward()');
+
+        await driver.pause(3000);
+    })
+
+    it.only('exercise test', async () => {
+
+        driver.startActivity(
+            "io.appium.android.apis",
+            "io.appium.android.apis.view.DateWidgets1"
+        );
+
+        await $('id=dateDisplay').waitForExist({ timeout: 3000 });
+
+        const dateDisplay = await $('id=dateDisplay');
+
+        await $('id=pickDate').click();
+
+        await $('android=new UiScrollable(new UiSelector().scrollable(true)).setAsHorizontalList().scrollForward()').click();
+
+        await $('android=new UiSelector().className("android.view.View").text("10")').click();
+
+        await $('id=android:id/button1').click();
+
+        // assertion
+        // extract from dateDisplay the day of month
+        const [month, day, year] = (await dateDisplay.getText()).split('-');
+        await expect(day).toBe('10');
+    })
 })
